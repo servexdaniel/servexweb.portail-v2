ed369cf596b7073b7c2f9e29443d031b116ac38e
Daniel <104506396+servexdaniel@users.noreply.github.com>
2025-12-09 14:27:39 -0500

    Mettre en place user.layout

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-user-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile user') }}
@@ -26,4 +26,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-user-layout>

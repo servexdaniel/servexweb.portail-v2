3f85b23c107ef0ab2ce6e45e7f1079e30c76a52f
Daniel <104506396+servexdaniel@users.noreply.github.com>
2025-12-09 11:48:52 -0500

    Ajouter le profile du contact

--- resources/views/contact/profile.blade.php
+++ resources/views/contact/profile.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-contact-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile contact') }}
@@ -9,21 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.update-profile-information-form />
+                    <livewire:contact.profile.update-profile-information-form />
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.update-password-form />
+                    <livewire:contact.profile.update-password-form />
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.delete-user-form />
+                    <livewire:contact.profile.delete-user-form />
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-contact-layout>
